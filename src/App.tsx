95fdc27e64e5c41cd20b8fb1d6ccdbb251c03842
Charlie Chenyu Zhang <charlie.chenyu.zhang@gmail.com>
2024-06-16 14:55:28 -0400

    working monaco text editor

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import { CodeEditor } from "./CodeEditor";
+import styled from "styled-components";
+import { Editor } from "./Editor";
+
+const AppContainer = styled.div``;
+const LiveblocksContainer = styled.div``;
+
+export default function App() {
+  const liveBlockKey = process.env.REACT_APP_LIVEBLOCKS_PROD_KEY || "";
+  const roomId = "my-room";
+  return (
+    <AppContainer>
+      <h1>Room ID: {roomId}</h1>
+      <div>
+        <LiveblocksProvider publicApiKey={liveBlockKey}>
+          <RoomProvider id={roomId} initialPresence={{}}>
+            {/* code editor */}
+            <ClientSideSuspense fallback={<div>Loading…</div>}>
+              {() => <CodeEditor />}
+            </ClientSideSuspense>
+
+            {/* text editor */}
+            {/* <ClientSideSuspense fallback={<div>Loading…</div>}>
+              {() => <Editor />}
+            </ClientSideSuspense> */}
+          </RoomProvider>
+        </LiveblocksProvider>
+      </div>
+    </AppContainer>
+  );
+}
